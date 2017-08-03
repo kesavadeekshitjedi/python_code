362f39f441ddc3d4eff1dfd94933a4252181a57f
Deekshit Addepalli <Deekshit Addepalli>
2017-08-03 17:13:15 -0500

    Fixed readjilfor command and condition code

--- AutoSysUtils/extractJobsFromJil/groupExtract/groupExtract/JMO_AE_Main.py
+++ AutoSysUtils/extractJobsFromJil/groupExtract/groupExtract/JMO_AE_Main.py
@@ -9,9 +9,11 @@ from collections import defaultdict
 import xlrd
 import JilUtilities
 from JilUtilities import readJil
+from JilUtilities import readJilForCommandAndCondition
 import groupExtract
 from groupExtract import *
-import cx_Oracle
+
+
 
 
 
@@ -20,36 +22,40 @@ def main():
     logger=logging.getLogger("JMO_AE_Utils.main")
     print("1. Read JIL File and create missing machine and resource defs")
     print("2. Read Excel sheet to create Top Level Boxes")
+    print("3. Read jil and extract command and condition for update_job file")
     
-    user_choice=input("Select an option (1) or (2).")
+    user_choice=input("Select an option (1) or (2) or (3).")
     if(user_choice=="1"):
    
         readJil("C:\\JMOFiles\\JOBS_____.Tranche4.jil","D:\JPMC-JMO\\scripted_outputFiles\\Resources.jil","D:\JPMC-JMO\\scripted_outputFiles\\Machines.jil")
         readJil("C:\\JMOFiles\\JOBS_____.ONDMD.Tranche4.jil","D:\JPMC-JMO\\scripted_outputFiles\\Resources_ONDMD.jil","D:\JPMC-JMO\\scripted_outputFiles\\Machines_ONDMD.jil")
         logger.info("Done")  
-    else:
-        if(user_choice=="2"):
-            readExcelForTopLevel("C:\\JMOFiles\\Tranche4JobstoBeConverted-PrebatchandOMNILoadshorty.xlsx","Sheet1")
-            print(type(jobsetsInTopBox))
-            keyList=jobsetsInTopBox.keys()
-            print(type(keyList))
-            boxStartTime="";
-            boxCalendar="";
-            myKeyList=list(keyList)
-            for key in myKeyList:
-                keyValues=jobsetsInTopBox[key]
-                for kvalue in keyValues:
-                    logger.debug("Value for Key: {0} is {1} ".format(key,kvalue))
-                    kvTuple=kvalue.split('-')
-                    logger.info(kvTuple)
-                    boxStartTime=kvTuple[1].strip()
-                    boxCalendar=kvTuple[2].strip()
-                    readJilForGroup("c:\\jmofiles\\JOBS_____.Tranche4.jil",kvTuple[0],key,kvTuple[2],kvTuple[1])
-                writeToFile("C:\\JMOFiles\\TopBoxFile.txt","insert_job: "+key)
-                writeToFile("C:\\JMOFiles\\TopBoxFile.txt","job_type: BOX")
-                writeToFile("C:\\JMOFiles\\TopBoxFile.txt","date_conditions: 1")
-                writeToFile("C:\\JMOFiles\\TopBoxFile.txt","start_times: "+boxStartTime)
-                writeToFile("C:\\JMOFiles\\TopBoxFile.txt","start_times: "+boxCalendar)
+    
+    if(user_choice=="2"):
+        readExcelForTopLevel("C:\\JMOFiles\\Tranche4JobstoBeConverted-PrebatchandOMNILoadshorty.xlsx","Sheet1")
+        print(type(jobsetsInTopBox))
+        keyList=jobsetsInTopBox.keys()
+        print(type(keyList))
+        boxStartTime="";
+        boxCalendar="";
+        myKeyList=list(keyList)
+        for key in myKeyList:
+            keyValues=jobsetsInTopBox[key]
+            for kvalue in keyValues:
+                logger.debug("Value for Key: {0} is {1} ".format(key,kvalue))
+                kvTuple=kvalue.split('-')
+                logger.info(kvTuple)
+                boxStartTime=kvTuple[1].strip()
+                boxCalendar=kvTuple[2].strip()
+                readJilForGroup("c:\\jmofiles\\JOBS_____.Tranche4.jil",kvTuple[0],key,kvTuple[2],kvTuple[1])
+            writeToFile("C:\\JMOFiles\\TopBoxFile.txt","insert_job: "+key)
+            writeToFile("C:\\JMOFiles\\TopBoxFile.txt","job_type: BOX")
+            writeToFile("C:\\JMOFiles\\TopBoxFile.txt","date_conditions: 1")
+            writeToFile("C:\\JMOFiles\\TopBoxFile.txt","start_times: "+boxStartTime)
+            writeToFile("C:\\JMOFiles\\TopBoxFile.txt","start_times: "+boxCalendar)
+    if(user_choice=="3"):
+        jilFileName=input("Enter the full path to the jil file to read")
+        readJilForCommandAndCondition(jilFileName)
 
     #readJil("c:\\jmofiles\\JOBS_____.Tranche4.jil","ns_ods_heartbeat","d68.am.prebatch.maint.base.main.box","base","18:45")
     #readJil("c:\\jmofiles\\JOBS_____.Tranche4.jil","ns_pbds_pentaho_carte_reboot","d68.am.prebatch.maint.base.main.box","base","18:45")
@@ -70,3 +76,4 @@ def main():
     #readJil("c:\\jmofiles\\JOBS_____.Tranche4.jil","ns_pbds_uscore_pb_sac","d68.am.prebatch.maint.usmffdsx.main.box","usmffdsx","18:45")
     logger.info("Done")
 
+main()
