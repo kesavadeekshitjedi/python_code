f1c8a1df587051fff1132f8d2ce57fc21f61869e
Deekshit Addepalli <daddepalli@robertmarktech.com>
2017-08-10 16:33:22 -0700

    Removed silly write

--- AutoSysUtils/extractJobsFromJil/groupExtract/groupExtract/groupExtract.py
+++ AutoSysUtils/extractJobsFromJil/groupExtract/groupExtract/groupExtract.py
@@ -81,8 +81,7 @@ def readExcelForTopLevel(topLevelFile,sheetName):
             
             logger.debug("Cell Data at {0},{1} is {2}".format(rowNum,colNum,cellData))
             print(jobsetsInTopBox)
-    for job in jobsetsInTopBox:
-        writeToFile("C:\\JMOFiles\\TopLevelBoxes_Jobsets.txt",job)
+    
             
 
 def readJilForFileWatchers(jilFileName,convertJobNames):
