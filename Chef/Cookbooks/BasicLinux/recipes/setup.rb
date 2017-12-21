b93def2c0c88972386d94d3059bfd5cc9c01be30
Deekshit Addepalli <daddepalli@robertmarktech.com>
2017-12-21 16:31:07 -0500

    added node attributes in the setup.rb file. Modified version informaton to 0.2.1

--- Chef/Cookbooks/BasicLinux/recipes/setup.rb
+++ Chef/Cookbooks/BasicLinux/recipes/setup.rb
@@ -10,3 +10,29 @@ end
 package 'emacs' do
 	action :install
 end
+
+file '/etc/motd' do
+	content "This server belongs to DK
+	HOSTNAME: #{node['hostname']}
+	IPADDRESS: #{node['ipaddress']}
+	TOTAL MEMORY: #{node['memory']['total']}
+	TOTAL SWAP  : #{node['memory']['swap']['total']}
+"
+	
+end
+# printing something using a variable
+# if we are using variables, then use double quotes around the string.
+# a variable is identified using the following way #{variable}
+apple_count =4
+puts "I have #{apple_count} apples"
+
+file '/var/www/html/index.html' do
+	content " Host information is as follows:
+	<p>
+	HOSTNAME: #{node['hostname']}
+        IPADDRESS: #{node['ipaddress']}
+        TOTAL MEMORY: #{node['memory']['total']}
+        TOTAL SWAP  : #{node['memory']['swap']['total']}
+	</p>
+"
+end
